3c4fd91fb404157a71c8f4191fb34cfe7754957e
Hollum <hollum@hotmail.com>
2020-03-05 12:36:12 +0100

    compressed code

--- node-docker-app/app.js
+++ node-docker-app/app.js
@@ -20,58 +20,23 @@ app.use(cors());
 //docker build -t hello-docker .
 //docker run -it -p 8888:4000 hello-docker
 
-app.get('/run/code', (req, res) => {
-    compile('g++ -o hello hello.cpp', () => {
-        //Run code
-        exec('./hello', (err, stdout, stderr) => {
-            if (err || stderr) {
-                console.error(`exec error: ${err}`);
-                res.json({output: "Det skjedde en feil, sjekk syntax."});
-                return;
-            }
-            console.log(`Kompilert kode: ${stdout}`);
-            res.json({output: stdout})
-
-        });
-    })
-
-});
-
-
 app.post('/run/code', (req, res) => {
     let code = req.body.input;
     fs.writeFile("hello.cpp", code, () => {
-        compile('g++ -o hello hello.cpp', () => {
-            //Run code
+        exec('g++ -o hello hello.cpp', (err, stdout, stderr) => {
+            if (err || stderr) {
+                console.error(`test error: ${err}`);
+                res.json({output: "Error: Syntax error"});
+                return;
+            }
             exec('./hello', (err, stdout, stderr) => {
-                if (err) {
-                    console.error(`exec error: ${err}`);
-                    res.json({output: err});
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    res.json({output: stderr});
-                    return;
-                }
                 console.log(`Kompilert kode: ${stdout}`);
                 res.json({output: stdout})
 
             });
-        })
+        });
     });
 });
-
-
-function compile(command, callback){
-    exec(command, (err, stdout, stderr) => {
-        if (err) {
-            console.error(`exec error: ${err}`);
-        }
-        callback();
-    });
-
-}
 
 
 app.listen(4000, function () {
--- node-docker-app/client/src/service/codeService.js
+++ node-docker-app/client/src/service/codeService.js
@@ -1,31 +1,8 @@
 import axios from "axios";
 
 export class CodeService {
-    static postCode(code){
-        let header = {
-            "Content-Type": "application/json",
-        };
-
-        axios.post('localhost:8888/code', {
-            code : code
-        }).catch(error => console.log(error))
-
-    }
-
-    static getOutput(){
-        return axios.get('http://localhost:8888/run/code');
-    }
-
     static sendInput(code){
         return axios.post('http://localhost:8888/run/code', {input : code});
     }
 
-    static getCode(callback){
-        let header = {
-            "Content-Type": "application/json",
-        };
-        axios.get('localhost:8888/code', {headers: header})
-            .then(response => callback(response))
-            .catch(error => console.log(error))
-    }
 }
